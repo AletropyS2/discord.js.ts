c75927c68f57ef445998ac9f48a6c845a6385fc3
AletropyS2 <nyancraftmain@gmail.com>
2022-11-30 16:23:25 -0300

    Changed package version

--- src/Commands/ping.ts
+++ src/Commands/ping.ts
@@ -7,7 +7,6 @@ export default {
     data: new SlashCommandBuilder()
     .setName("ping")
     .setDescription("Responds bot latency"),
-    permissions: "Administrator",
     run: async (client, interaction) => {
 
         const embed = new EmbedBuilder()
